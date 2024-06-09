66e1c0c9c049405001b5ce0ec1b6e1c6ad137534
usamasadiqus <usamasadiq029@gmail.com>
2024-06-09 14:54:02 +0500

    setup dynamodb, lambda and api gateway for user and added one handler for getAllUsers with read and write access

--- lib/aws-cdk-demo-todos-stack.ts
+++ lib/aws-cdk-demo-todos-stack.ts
@@ -1,16 +1,39 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
 export class AwsCdkDemoTodosStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB Users Table
+    const usersTable = new Table(this, "users", {
+      partitionKey: { name: "id", type: AttributeType.STRING },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkDemoTodosQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda Functions
+    const getAllUsers = new Function(this, "GetAllUsersLambdaHandler", {
+      runtime: Runtime.NODEJS_18_X,
+      code: Code.fromAsset("functions"),
+      handler: "users.getAllUsersHandler",
+      environment: {
+        USERS_TABLE_NAME: usersTable.tableName,
+      },
+    });
+
+    usersTable.grantReadWriteData(getAllUsers);
+
+    // Create api gateway methods and path
+    const api = new RestApi(this, "user-apis");
+
+    api.root
+      .resourceForPath("users")
+      .addMethod("GET", new LambdaIntegration(getAllUsers));
+
+    new cdk.CfnOutput(this, "APIURL", {
+      value: api.url ?? "Something went wrong",
+    });
   }
 }
